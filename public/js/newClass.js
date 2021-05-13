0244aa871ac2adb623ad1534292a9eb1bd514cb2
EliSzafranski <Eszafranski101@gmail.com>
2021-05-13 13:28:33 -0400

    front end new class validation

--- public/js/newClass.js
+++ public/js/newClass.js
@@ -1,12 +1,11 @@
-document.getElementById("newClassForm").addEventListener("submit", (event) =>{
-    event.preventDefault();
+function checkForm(){
     const courseSeason = $("#semester").val()[0];
     const courseYear = $("#semester").val()[1];
-    const fromTime = $("#fromTime").val();
-    const toTime = $("#toTime").val();
-    const className = $("#className").val();
-    const capacity = $("#classCapacity").val();
-    const courseNum = $("#courseNum").val();
+    const fromTime = $("#fromTime");
+    const toTime = $("#toTime");
+    const className = $("#className");
+    const capacity = $("#classCapacity");
+    const courseNum = $("#courseNum");
     const instructor = $("#courseInstructor").val();
     const department = $("#dept").val();
     const courseDesc = $("#courseDesc").val();
@@ -16,25 +15,90 @@ document.getElementById("newClassForm").addEventListener("submit", (event) =>{
     const weBtn = $("#weBtn").val();
     const thBtn = $("#thBtn").val();
     const frBtn = $("#frBtn").val();
-    $.post('/api/createClass',{
-        season: courseSeason,
-        year: courseYear,
-        fromTime: fromTime,
-        toTime: toTime,
-        name: className,
-        capacity: capacity,
-        number: courseNum,
-        instructor: instructor,
-        department: department,
-        courseDesc: courseDesc,
-        enrollmentDate: enrollmentDate,
-        moBtn: moBtn,
-        tuBtn: tuBtn,
-        weBtn: weBtn,
-        thBtn: thBtn,
-        frBtn: frBtn
-    })
-    .done((data) =>{
-        console.log(data);
-    })
+    const timeBlock = $("#timeBlock");
+    var foundErrors = false; // This is used to check if there are any errors. If there are, this is set to true and we don't POST the form
+    var posErrors = {
+        invName:"Invalid Course Name", 
+        invCapacity:"Invalid capacity",
+        invCourseNum:"Invalid Course Number",
+        invDepartment:"Invalid Department",
+        invTimes:"'From' must be before 'To'"
+    };
+
+    const inputName = /[a-zA-Z]{2,}/;
+    const inputCap = /\d{1,3}/;
+    if(!inputName.test(className.val())){
+        handleError(className, posErrors.invName);
+        foundErrors = true;
+    }
+    else{
+        handleSuccess(className);
+    }
+
+    if(!inputCap.test(capacity.val())){
+        handleError(capacity, posErrors.invCapacity);
+        foundErrors = true;
+    }
+    else{
+        handleSuccess(capacity);
+    }
+
+    if(!inputCap.test(courseNum.val())){
+        handleError(courseNum, posErrors.invCourseNum);
+        foundErrors = true;
+    }
+    else{
+        handleSuccess(courseNum);
+    }
+    console.log(fromTime.val() >= toTime.val());
+    if(fromTime.val() >= toTime.val()){
+        handleError(timeBlock, posErrors.invTimes);
+        foundErrors = true;
+    }
+    else{
+        handleSuccess(timeBlock);
+    }
+    if(!foundErrors){
+        $.post('/api/createClass',{
+            season: courseSeason,
+            year: courseYear,
+            fromTime: fromTime.val(),
+            toTime: toTime.val(),
+            name: className.val(),
+            capacity: capacity.val(),
+            number: courseNum.val(),
+            instructor: instructor,
+            department: department,
+            courseDesc: courseDesc,
+            enrollmentDate: enrollmentDate,
+            moBtn: moBtn,
+            tuBtn: tuBtn,
+            weBtn: weBtn,
+            thBtn: thBtn,
+            frBtn: frBtn
+        })
+        .done((data) =>{
+            console.log(data);
+        })
+    }
+}
+
+document.getElementById("newClassForm").addEventListener("submit", (event) =>{
+    event.preventDefault();
+    checkForm();
 });
+
+function handleError(elmt, err){
+    elmt.removeClass("success");
+    elmt.addClass("error");
+    elmt.next().removeClass("success");
+    elmt.next().addClass("error");
+    elmt.next().text(err);
+}
+
+function handleSuccess(elmt){
+    elmt.removeClass("error");
+    elmt.addClass("success");
+    elmt.next().removeClass("error");
+    elmt.next().addClass("success");
+}
